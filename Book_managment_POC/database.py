263da56a236830f86f0453727a3a335e6d7deed5
Abhignyakumar <abhignya.mudumbai@thezennialpro.com>
2025-07-03 18:51:16 +0530

    Adding my POC with 5 endpoints

---
+++ Book_managment_POC/database.py
@@ -0,0 +1,7 @@
+from models import Book
+
+# Simulated DB: a list of Book objects
+books_db = [
+    Book(id=1, title="The Alchemist", author="Paulo Coelho", pages=208),
+    Book(id=2, title="1984", author="George Orwell", pages=328),
+]
